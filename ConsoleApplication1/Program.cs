fada30413dc175a284db080eba2e205b242825ef
a1exv <volodkevich.alex@gmail.com>
2017-01-11 22:18:31 +0300

    lab1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite koordinati 3 tochek");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            double y1 = Convert.ToInt32(Console.ReadLine());
+            double x2 = Convert.ToInt32(Console.ReadLine());
+            double y2 = Convert.ToInt32(Console.ReadLine());
+            double x3 = Convert.ToInt32(Console.ReadLine());
+            double y3 = Convert.ToInt32(Console.ReadLine());
+            double a=Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
+            double b = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
+            double c = Math.Sqrt(Math.Pow((x1 - x3), 2) + Math.Pow((y1 - y3), 2));
+            double p = a + b + c;
+            double halfp = p / 2;
+            double s = Math.Sqrt(halfp * (halfp - a) * (halfp - b) * (halfp - c));
+            Console.WriteLine("Perimetr = {0},  Area= {1}", p, s);
+        }
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число от 100 до 999");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 100 && number < 999)
+            {
+                String[] sotni = new String[9] { "сто ", "двести ", "триста ", "четыреста ", "пятьсот ", "шестьсот ", "семьсот ", "восемьсот ", "девятьсот " };
+                String[] desyatki = new String[9] { "десять ", "двадцать ", "тридцать ", "сорок ", "пятьдесят ", "шестьдесят ", "семьдесят ", "восемьдесят ", "девяносто " };
+                String[] nadcat = new String[9] { "одинадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
+                String[] edin = new String[9] { "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+                int sto = number / 100;
+                number = number % 100;
+                Console.Write(sotni[sto - 1]);
+                if (number > 19)
+                {
+                    int desyat = number / 10;
+                    Console.Write(desyatki[desyat - 1]);
+                    number = number % 10;
+                    if (number > 0)
+                    {
+                        Console.Write(edin[number - 1]);
+                    }
+                }
+                else if (number > 10 && number < 20)
+                {
+                    number = number % 10;
+                    Console.Write(nadcat[number - 1]);
+                }
+                else if (number == 10)
+                {
+                    Console.Write(desyatki[0]);
+                }
+                else if (number == 0) { }
+                else Console.Write(edin[number - 1]);
+                Console.WriteLine("\n");
+            }
+            else Console.WriteLine("Nevernoe chislo!");
+        }
+    }
+}
